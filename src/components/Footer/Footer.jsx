d8944a6f9dfbd390c2e2090254413752afa0a60c
Layesall <contact@layesall.com>
2022-04-22 22:21:14 +0200

    Footer built and designed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,60 @@
 import React from "react";
+import { Col, Container, Image, Row } from "react-bootstrap";
+import {
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaGooglePlusSquare,
+  FaYoutube,
+} from "react-icons/fa";
+import Config from "../../config/Config";
+import Logo from "../../assets/logo/logo-light.png";
 import "./Footer.css";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <footer className="footer-component">
-      <div className="footer-top"></div>
+      <Container fluid className="footer-top">
+        <Row className="footer-top">
+          <Col md={6}>
+            <div className="footer-about">
+              <Image src={Logo} alt="logo Lifeshoot" className="footer-logo" />
+              <p>
+                Pastry cheesecake marzipan ice cream dessert carrot cake toffee
+                jelly-o. Caramels macaroon caramels topping sweet roll cupcake
+                bear claw marshmallow. Tootsie roll sugar plum chocolate muffin
+                cake biscuit. Gummies liquorice marshmallow cheesecake powder
+                bonbon marzipan candy canes apple pie. Bonbon sugar plum candy
+                canes pastry bonbon carrot cake. Chocolate cake bear claw marzipan
+                jelly-o chocolate gingerbread dragée biscuit candy. Powder bear
+                claw jelly beans sesame snaps shortbread muffin. Pie jelly beans
+                chocolate bear claw pudding sesame snaps marshmallow.
+              </p>
+            </div>
+          </Col>
+          <Col md={6}>
+            <div className="footer-media">
+              <Link to="" className="media-link">
+                <FaInstagramSquare />
+              </Link>
+              <Link to="" className="media-link">
+                <FaFacebookSquare />
+              </Link>
+              <Link to="" className="media-link">
+                <FaGooglePlusSquare />
+              </Link>
+              <Link to="" className="media-link">
+                <FaYoutube />
+              </Link>
+            </div>
+          </Col>
+        </Row>
+      </Container>
       <div className="footer-copyright">
-        &copy; NIKOU ONE &middot; 2022 | by{" "}
-        <a href="www.layesall.com" className="footer-author">
+        &copy; {Config.AppName} &middot; 2022 | by 
+        <Link to="https://layesall.com/" className="footer-author" title="Layesall">
           Layesall
-        </a>
+        </Link>
       </div>
     </footer>
   );
