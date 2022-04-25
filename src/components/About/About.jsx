5569f0a8aec0f770724e05cce810d0d824c5a92c
Layesall <contact@layesall.com>
2022-04-25 15:58:08 +0200

    About content added and Contact form fix

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -20,24 +20,13 @@ export default function About() {
           <Col md={8}>
             <h1 className="about-title">Who is behind the lens</h1>
             <p className="about-text">
-              Gingerbread chocolate bar candy gummi bears pastry jelly powder.
-              Bear claw ice cream powder chupa chups halvah muffin jelly-o
-              macaroon. Lemon drops sesame snaps lollipop macaroon muffin chupa
-              chups chocolate cake ice cream halvah. Dessert dessert soufflé
-              soufflé jelly pudding sesame snaps. Gummi bears chocolate bar
-              pastry dessert bonbon lemon drops. Candy canes macaroon chocolate
-              cake apple pie halvah marzipan jelly-o icing.
+              I entered the photography world in 2016. But before that, I was a model for 2 years. I was used to be in front of the camera, but I wanted to know what it felt like to be behind the camera. That's when my passion for photography began.
             </p>
             <p className="about-text">
-              Pastry cheesecake marzipan ice cream dessert carrot cake toffee
-              jelly-o. Caramels macaroon caramels topping sweet roll cupcake
-              bear claw marshmallow. Tootsie roll sugar plum chocolate muffin
-              cake biscuit. Gummies liquorice marshmallow cheesecake powder
-              bonbon marzipan candy canes apple pie. Bonbon sugar plum candy
-              canes pastry bonbon carrot cake. Chocolate cake bear claw marzipan
-              jelly-o chocolate gingerbread dragée biscuit candy. Powder bear
-              claw jelly beans sesame snaps shortbread muffin. Pie jelly beans
-              chocolate bear claw pudding sesame snaps marshmallow.
+              I started by myself as a freelancer, but in 2018 I started having photography courses to understand more about my camera. With the knowledge I have learnt nd the experience I have acquired, I am sure that I will do my best to bring your wishes into reality.
+            </p>
+            <p className="about-text">
+              My sincere satisfaction comes from seeing my client's individual style emerge, while I am helping them bring their own vision into life. I hope you will allow me to point the picture you have dreamt in your heart.
             </p>
             <h3 className="about-signature"> Lifeshoot Moments </h3>
           </Col>
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,41 +1,48 @@
-import React from 'react';
-import { Button, Col, Container, Form, Row } from 'react-bootstrap';
-import { FaEnvelope , FaPhoneAlt} from 'react-icons/fa';
+import React from "react";
+import { Button, Form} from "react-bootstrap";
 
-import './Contact.css';
+import "./Contact.css";
 
 export default function Contact() {
-  return (
-    <div id='contact' className='contact-component'>
-      
-      <Container>
-        <Row className='contact-info'>
-          <Col md={6}> 
-            <h1 className='contact-title'>Contact me</h1> 
-            <p className='contact-intro'>Let me light up your world with my natural flashlight.</p>
-            <Form>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Control type="text" placeholder="Your name" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Control type="email" placeholder="Your e-mail" />
-              </Form.Group>
-              <Form.Select aria-label="Default select example" name='services' className='mb-3'>
-                <option>Object</option>
-                <option value="portrait">Portrait</option>
-                <option value="ceremony">Ceremony</option>
-                <option value="baby">Babyborn</option>
-                <option value="autres">Autres</option>
-              </Form.Select>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                <Form.Control as="textarea" rows={3} placeholder="Here your message..." />
-              </Form.Group>
-              <Button variant="dark" className='button-send'> Send </Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
 
+  return (
+    <div id="contact" className="contact-component">
+      <div className="contact-card">
+        <h1 className="contact-title">Contact me</h1>
+        <p className="contact-intro">
+          When you want to do any kind of Photoshoot for your professional or
+          personal use, please don't hesitate to contact me and let me light up
+          your world with my natural flashlight.
+        </p>
+        <Form id="contact-form">
+          <Form.Group className="mb-3">
+            <Form.Control type="text" placeholder="Your name" />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Control type="email" placeholder="Your e-mail" />
+          </Form.Group>
+          <Form.Select name="services" className="mb-3">
+            <option>Object</option>
+            <option value="portrait">Portrait</option>
+            <option value="ceremony">Ceremony</option>
+            <option value="baby">Babyborn</option>
+            <option value="autres">Autres</option>
+          </Form.Select>
+          <Form.Group className="mb-3">
+            <Form.Control
+              as="textarea"
+              rows={3}
+              placeholder="Here your message..."
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <div className="form-alert-message invisible">Thanks for your message ! </div>
+            <Button type="button" variant="dark" className="button-send">
+              Send
+            </Button>
+          </Form.Group>
+        </Form>
+      </div>
     </div>
   );
 }
